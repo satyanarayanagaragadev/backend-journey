ed59a757f289eeb5fd68b41237f4bdd93833a7c6
satyanarayanagaragadev <satya.narayana.garaga.dev@gmail.com>
2026-03-10 07:05:12 +0530

    variables

---
+++ day01/Variables.py
@@ -0,0 +1,54 @@
+#Create variables to store your name, age, and city and print them.
+name = "satyanarayana"
+age = 19
+city = "Duvva"
+
+print(name , age , city)
+
+
+#Create two variables with numbers and swap their values.
+x = 6
+y = 9
+
+x , y = y , x
+
+print(x,y)
+
+
+#Create two number variables and print their sum, difference, product, and division.
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+
+
+#Create a score variable starting at 0. Add 10 to it, then add 5 more. Print the final score.
+score  = 0
+score  = score + 10
+score = score + 5
+
+print(score)
+
+
+#Create a name and age variable and print a sentence combining both.
+print(f"my name is {name} and my age is {age}")
+
+
+#Create length and width variables and calculate the area of a rectangle.
+length = 10
+width  = 20
+print(length * width)
+
+
+#Store a temperature in Celsius and convert it to Fahrenheit using a formula.
+celsius  = 37 
+fahrenheit = (celsius * 9/5)+ 32
+
+print(fahrenheit)
+
+
+#Store principal, rate, and time variables and calculate simple interest.
+principal  = 10000
+rate = 18
+time = 1
+interest = (principal * rate * time) / 100
