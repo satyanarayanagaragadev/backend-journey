c8dd17547420138825c87a32027af0ad8ce6f414
satyanarayanagaragadev <satya.narayana.garaga.dev@gmail.com>
2026-03-13 14:26:26 +0530

    Add files via upload

---
+++ day01/day_1_project.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+city = input("Enter your city: ")
+
+print("Name : ", name)
+print("Age : ", age)
+print("Height : ", height)
+print("City : ", city)
